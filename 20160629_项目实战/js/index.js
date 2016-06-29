4731dc6e99385a15ef9255865325d91c6e8a2cf6
zhouxiaotian <1282347298@qq.com>
2016-06-29 18:37:24 +0800

    项目实战

--- 20160629_项目实战/js/index.js
+++ 20160629_项目实战/js/index.js
@@ -10,3 +10,18 @@ $(function () {
         });
     });
 });
+
+//->JS控制SECTION区域的高度
+$(function () {
+    //->通过jQuery的css方法获取的结果是加上单位的,我们计算的话需要把单位去除掉
+    change();
+    function change() {
+        var $section = $(".section");
+        var curH = $(window).outerHeight() - $(".header").outerHeight() - parseFloat($section.css("marginTop")) - parseFloat($section.css("marginBottom"));
+        $section.css({
+            height: curH
+        });
+    }
+
+    $(window).on("resize", change);
+});
